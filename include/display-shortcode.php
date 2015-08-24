f47f15d29a8ea473ef9c3bbff518657d788cedf3
Fabio Mattei <burattino@gmail.com>
2015-08-24 20:00:40 +0200

    shortcake is finally working

--- include/display-shortcode.php
+++ include/display-shortcode.php
@@ -1,29 +1,31 @@
 <?php
 
 /**
- * adding shortcode to display the events
+ * This function handle the short code
  */
-function RCEV_events_list($atts, $content){
-	
+function RCEV_events_list( $atts, $content ) {
 	global $post;
 	
-	extract( shortcode_atts( array( // a few default values
-		'posts_per_page' => '1',
-		'post_type' => 'film',
-		'caller_get_posts' => 1)
-		, $atts));
-
-	$posts = new WP_Query($atts);
-	$output = '';
+	$atts = array( // a few default values
+			'posts_per_page' => '4',
+			'post_type' => RCEV_SLUG
+			);
+			
+	$posts = new WP_Query( $atts );
+	$out = '';
 	
-	if ($posts->have_posts())
-	    while ($posts->have_posts()):
+    ob_start();
+	
+	if ($posts->have_posts()) {
+		
+	    while ($posts->have_posts()) {
 	        $posts->the_post();
-	        $out = '<div class="film_box">
-	            <h4>Film Name: <a href="'.get_permalink().'" title="' . get_the_title() . '">'.get_the_title() .'</a></h4>
-	            <p class="Film_desc">'.get_the_content().'</p>';
+			
+	        $out .= '<div class="events_box">
+	            <h4><a href="'.get_permalink().'" title="' . get_the_title() . '">'.get_the_title() .'</a></h4>
+	            <p class="event_desc">'.get_the_content().'</p>';
 	            // add here more...
-	        $out .='</div>';
+	        $out .= '</div>';
 			
 	/* these arguments will be available from inside $content
 	    get_permalink()  
@@ -34,13 +36,17 @@ function RCEV_events_list($atts, $content){
 	    get_post_meta($post->ID, 'field_name', true);
 	*/
 	
-		endwhile;
-	else
+		} // end while loop
+		
+	} else {
 		return; // no posts found
+	}
 
-	wp_reset_query();
-	return html_entity_decode($out);
+	echo $out;
+	
+    return ob_get_clean();
 }
 add_shortcode( 'RCEVList', 'RCEV_events_list' );
 
-// usage [RCEVList p=FILM_POST_ID]
+
+// usage [RCEVList]
